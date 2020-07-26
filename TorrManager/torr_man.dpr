31afd2665a880b3bd4e8bca4cdf4537e38d96c77
Sergio <serginhoshm@gmail.com>
2020-07-26 12:01:18 -0300

    Move files based on size,delete empty dirs

--- TorrManager/torr_man.dpr
+++ TorrManager/torr_man.dpr
@@ -5,25 +5,62 @@ program torr_man;
 uses
   SysUtils,
   Classes,
+  IniFiles,
+  Windows,
   u_lib in 'u_lib.pas';
 
 var
   Lista: TStringList;
   i: integer;
+  afilesize: int64;
+  afile,
+  anewfilename: string;
+  config:TIniEntry;
 begin
   { TODO -oUser -cConsole Main : Insert code here }
-  Writeln('Teste');
-  Lista := TStringList.Create;
   try
-    ListDir('C:\Windows', Lista);
-    for i:= 0 to Lista.Count -1 do
-    begin
-      Writeln(Lista.Strings[i]);
-    end;
-  finally
-    FreeAndNil(Lista);
-  end;
 
-  //ListDir('dddd');
+    Writeln('Teste');
+    Lista := TStringList.Create;
+    try
+      ReadIniConfig('MoverFilmesConcluidos', config);
+      Writeln('InputDir = ' + config.InputDir);
+      Writeln('OutputDir = ' + config.OutputDir);
+      Writeln('MinFileSize = ' + IntToStr(config.MinFileSize));
+      Writeln('DeleteFilesBelowMin = ' + IntToStr(config.DeleteFilesBelowMin));
+      Writeln('DeleteEmptyDirs = ' + IntToStr(config.DeleteEmptyDirs));
+
+      //ListDir('L:\filmes', Lista);
+      ListDir(config.InputDir, Lista);
+      for i:= 0 to Lista.Count -1 do
+      begin
+        afile := Lista.Strings[i];
+        afilesize := GetFileSize(afile);
+
+        if afilesize >= config.MinFileSize then
+        begin
+          Writeln('Keep file ' + afile);
+          anewfilename := IncludeTrailingPathDelimiter(config.OutputDir) + ExtractFileName(afile);
+          MoveFile(PChar(afile),PChar(anewfilename));
+        end
+        else
+        begin
+         // Writeln('Delete File ' + afile);
+         if ExtractFileName(afile) <> '.config' then
+          DeleteFile(PChar(afile));
+        end;
+      end;
+      if config.DeleteEmptyDirs >=1 then
+        DelEmptyFolders(IncludeTrailingPathDelimiter(config.InputDir));
+
+    finally
+      FreeAndNil(Lista);
+    end;
+  except
+    On E:Exception do
+    begin
+      Writeln(E.message);
+    end;
+  end;
   Readln;
 end.
--- TorrManager/u_lib.pas
+++ TorrManager/u_lib.pas
@@ -4,12 +4,24 @@ interface
 
 uses
   SysUtils,
-  Classes;
+  Classes,
+  IniFiles;
+
+type
+  TIniEntry = record
+    InputDir: string;
+    OutputDir: string;
+    MinFileSize: Int64;
+    DeleteFilesBelowMin: integer;
+    DeleteEmptyDirs: Integer;
+  end;
 
   procedure ListDir(Dir: string; var List: TStringList);
+  function GetFileSize(fileName : wideString) : Int64;
+  procedure ReadIniConfig(sectionName: string; var IniConf: TIniEntry);
+  procedure DelEmptyFolders(dir: string);
 
 implementation
-
 
 
 procedure ListDir(Dir: string; var List: TStringList);
@@ -33,25 +45,66 @@ begin
   end;
 end;
 
+function GetFileSize(fileName : wideString) : Int64;
+var
+  sr : TSearchRec;
+begin
+  if FindFirst(fileName, faAnyFile, sr ) = 0 then
+    result := Int64(sr.FindData.nFileSizeHigh) shl Int64(32) + Int64(sr.FindData.nFileSizeLow)
+  else
+    result := -1;
+  FindClose(sr);
+end;
+
+procedure ReadIniConfig(sectionName: string; var IniConf: TIniEntry);
+var
+  aIniFilename: string;
+  aIni: TIniFile;
+begin
+  aIniFilename := IncludeTrailingPathDelimiter(ExtractFilePath(ParamStr(0))) + 'config.ini';
+
+  Writeln(aIniFileName);
+  if not FileExists(aIniFilename) then
+    raise Exception.Create('File not found: config.ini');
+
+  try
+    aIni := TIniFile.Create(aIniFilename);
+    if not aIni.SectionExists(sectionName)then
+      raise Exception.Create('Section not found on INI file: ' + sectionName);
+
+    IniConf.InputDir := aIni.ReadString(sectionName, 'InputDir', '');
+    IniConf.OutputDir := aIni.ReadString(sectionName, 'OutputDir', '');
+    IniConf.MinFileSize := aIni.ReadInteger(sectionName, 'MinFileSize', 100);
+    IniConf.DeleteFilesBelowMin := aIni.ReadInteger(sectionName, 'DeleteFilesBelowMin', 0);
+    IniConf.DeleteEmptyDirs := aIni.ReadInteger(sectionName, 'DeleteEmptyDirs', 0);
+  finally
+    FreeAndNil(aIni);
+  end;
+
+end;
+
+procedure DelEmptyFolders(dir: string);
+var
+ Search : TSearchRec;
+ ok: integer;
+begin
+ ok := FindFirst(dir + '\*.*',faDirectory,Search);
+ while ok = 0 do
+ begin
+   if ((Search.Name <> '.' ) and (Search.Name <> '..')) then
+   begin
+      DelEmptyFolders(dir + '\' + Search.Name);
+   end;
+   ok := FindNext(Search);
+ end;
+
+ FindClose(Search);
+ try
+   RmDir(dir);
+ except
+   // do nothing - dir is not empty
+ end;
+end;
+
 end.
 
-
-
-
-
-procedure TForm1.AddAllFilesInDir(const Dir: string);
-var
-  SR: TSearchRec;
-begin
-  if FindFirst(IncludeTrailingBackslash(Dir) + '*.*', faAnyFile or faDirectory, SR) = 0 then
-    try
-      repeat
-        if (SR.Attr and faDirectory) = 0 then
-          ListBox1.Items.Add(SR.Name)
-        else if (SR.Name <> '.') and (SR.Name <> '..') then
-          AddAllFilesInDir(IncludeTrailingBackslash(Dir) + SR.Name);  // recursive call!
-      until FindNext(Sr) <> 0;
-    finally
-      FindClose(SR);
-    end;
-end;
